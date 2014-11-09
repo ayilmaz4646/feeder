9980b4bed3f1c0e789500d55440020b0b457b99f
Ahmet Yilmaz <ayilmaz4646@gmail.com>
2014-11-09 18:09:57 +0200

    modified

--- app/controllers/feeder/feeds_controller.rb
+++ app/controllers/feeder/feeds_controller.rb
@@ -13,5 +13,28 @@ module Feeder
     	@feed = @feed_source.feeds.find(params[:id])
     end
 
+    def create
+    	@feed_source = Feed_source.find(param[:feed_source_id])
+    	@feed = @feed_source.feeds.new(feed_params)
+
+    	if @feed.save
+      	redirect_to @feed, notice: 'Başarıyla Oluşturuldu'
+    	else
+      	render :new
+    	end
+		end
+
+		def destroy
+			@feed_source = Feed_source.find(param[:feed_source_id])
+    	@feed = @feed_source.feeds.find(params[:id])
+    	@feed.destroy
+		end
+
+    private
+    
+    def feed_params
+      params.require(:feed).permit(:title, :content, :url, :analyzed, :language)
+    end
+
   end
 end
