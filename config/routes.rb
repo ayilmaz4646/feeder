568a0373e97f3e659ec6c0fe279aa77e1df1ed10
Ahmet Yilmaz <ayilmaz4646@gmail.com>
2014-11-09 17:39:08 +0200

    created feed_source and feed 's controller and model pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Feeder::Engine.routes.draw do
+  get 'feeds/new'
+
+  get 'feeds/edit'
+
+  get 'feeds/show'
+
+  get 'feed_sources/new'
+
+  get 'feed_sources/edit'
+
   get 'home/index'
 
 	root :to => "home#index"
