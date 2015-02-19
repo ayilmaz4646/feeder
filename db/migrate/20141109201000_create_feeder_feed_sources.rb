173967886fc92d8c66c4b0e030f25f72c4dd0fb4
Ahmet Yilmaz <ayilmaz4646@gmail.com>
2015-02-19 19:15:56 +0200

    modified

--- db/migrate/20141109201000_create_feeder_feed_sources.rb
+++ db/migrate/20141109201000_create_feeder_feed_sources.rb
@@ -3,7 +3,7 @@ class CreateFeederFeedSources < ActiveRecord::Migration
     create_table  :feeder_feed_sources do |t|
 
       t.string    :title, null: false
-      t.string    :url
+      t.string    :url, unique: true
   		t.string    :duration
   		t.datetime  :last_check_time
       t.timestamps
